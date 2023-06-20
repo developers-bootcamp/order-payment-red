96bcddb49f6e3cfaed6248a5e51f8988be40779d
Ruti Churi <r0527694893@gmail.com>
2023-06-20 09:40:59 +0300

    first commit

---
+++ src/main/java/com/sap/orderpaymentred/OrderPaymentRedApplication.java
@@ -0,0 +1,13 @@
+package com.sap.orderpaymentred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPaymentRedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPaymentRedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sap/orderpaymentred/OrderPaymentRedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.orderpaymentred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPaymentRedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
